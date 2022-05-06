67959e2c7f274dab9e4c38d92b0cd8e983b8fb7a
khoapnn21 <82403996+khoapnn21@users.noreply.github.com>
2022-05-06 15:01:15 +0700

    Update submain.js
    update

--- submain.js
+++ submain.js
@@ -3,7 +3,7 @@ window.addEventListener("message", subDisplay,false );
 
 function subDisplay(e) {
     
-    if (e.origin !== "http://127.0.0.1:5500") return;
+    if (e.origin !== "https://khoapnn21.github.io/index.html") return;
     document.getElementById("subDisplayMsg").innerHTML += e.data ;
     
 }
@@ -12,8 +12,8 @@ function sendMess() {
     let contentMess = document.getElementById("subTextMess").value
     if(contentMess === '') return;
     
-    let newWin = window.open("http://127.0.0.1:5500/index.html", "index")
-    newWin.postMessage(contentMess, "http://127.0.0.1:5500/index.html");	
+    let newWin = window.open("https://khoapnn21.github.io/index.html", "index")
+    newWin.postMessage(contentMess, "https://khoapnn21.github.io/index.html");	
 };
     
 
