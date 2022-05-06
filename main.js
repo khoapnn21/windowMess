43467534352cfb652554cf1dae3a9004b848cc4f
khoapnn21 <82403996+khoapnn21@users.noreply.github.com>
2022-05-06 15:00:16 +0700

    Update main.js
    update

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function sendMess() {
     let contentMess = document.getElementById("textMess").value
     if(contentMess === '') return;
     
-    let newWin = window.open("http://127.0.0.1:5500/subIndex.html", "subIndex")
-    newWin.postMessage(contentMess, "http://127.0.0.1:5500/subIndex.html");	
+    let newWin = window.open("https://khoapnn21.github.io/subIndex.html", "subIndex")
+    newWin.postMessage(contentMess, "https://khoapnn21.github.io/subIndex.html");	
 
 
 };
@@ -11,7 +11,7 @@ function sendMess() {
 window.addEventListener("message", parentDisplay,false );
 
 function parentDisplay(e) {
-    if (e.origin !== "http://127.0.0.1:5500") return;
+    if (e.origin !== "https://khoapnn21.github.io/index.html") return;
     document.getElementById("displayMsg").innerHTML += e.data  ;
     
 }
